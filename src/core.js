0f91e98009aa0d4b2bb3e111b1762892ede875df
YoshiyukiKato <yo.shuuuuki@gmail.com>
2018-08-20 16:08:20 +0900

    fix bug

--- src/core.js
+++ src/core.js
@@ -1,15 +1,15 @@
 const Context = require("./context/context");
 const {launchChromes} = require("./runner/launch-chrome");
 const {execLighthouse} = require("./runner/exec-lighthouse");
-const defaultChromeConfig = require("./runner/config/chrome-config");
+const defaultPuppeteerConfig = require("./runner/config/puppeteer-config");
 const defaultLighthouseConfig = require("./runner/config/lighthouse-config");
 
-exports.collectPerf = function({targets, reporters, chromeNum, chromeConfig, lighthouseConfig}){
+exports.collectPerf = function({targets, reporters, chromeNum, puppeteerConfig, lighthouseConfig}){
   const context = new Context(targets, reporters);
-  const chConfig = Object.assign(defaultChromeConfig, chromeConfig);
+  const ppConfig = Object.assign(defaultPuppeteerConfig, puppeteerConfig);
   const lhConfig = Object.assign(defaultLighthouseConfig, lighthouseConfig);
   const work = execLighthouse.bind(null, lhConfig, context);
-  const workers = launchChromes(chConfig, chromeNum).map((launchChrome) => {
+  const workers = launchChromes(ppConfig, chromeNum).map((launchChrome) => {
     return launchChrome.then(work);
   })
   return Promise.all(workers).then(context.close.bind(context));
